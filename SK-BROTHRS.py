9e8c62021d2cc603e0d2a39b11be17798a0a18e9
BDTIGER7151 <69599704+BDTIGER7151@users.noreply.github.com>
2020-09-10 03:18:01 +0800

    Create SK-BROTHRS.py
    apt update
    apt upgrade
    pkg install git
    pkg install python2
    pkg install python
    pip2 install mechanize
    pip2 install requests
    git clone https://github.com/BDTIGER7151/SK-BROTHERS.git
    cd SK-BROTHERS
    ls
    python2 SK-BROTHERS.py

---
+++ SK-BROTHRS.py
@@ -0,0 +1,35 @@
+# I-Love-You-Virus Note: 
+This virus is Harmless and can only shutdown the PC...
+
+But I will not be responsible for anything if anything goes wrong with the PC....
+
+Wanna Prank someone You love...
+
+Then Why don't You try this video...
+
+In this video I have made a simple virus using notepad...
+
+Just Open Notepad in your PC...
+
+And type the following codes...
+
+## Setup Video-
+[![facebook group]( https://www.facebook.com/groups/PrimeNews24/?ref=share  ( https://www.facebook.com/groups/PrimeNews24/?ref=share )
+
+# Setup-
+
+After typing, Save the file as Love.bat
+Now give this file to the person you want through pendrive or as an attachment and convince them to execute this file....
+
+This virus is harmless and can only shutdown their PC if they type "NO" when the program asks them....
+But I will not be responsible for anything if anything Goes wrong with their PC...
+
+## ðŸ”— ***Check this***
+
+### join my FB group :
+https://www.facebook.com/groups/PrimeNews24/?ref=share
+
+### Chekout our webite:
+https://h1ckpro.tk
+
+## ðŸ‘¥ ***Join***
